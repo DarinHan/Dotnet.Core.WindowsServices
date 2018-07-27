5e820c73d28d28f5bb2569137a4d9a3abb0f0f70
DarinHan <2144543221@qq.com>
2018-07-27 18:44:22 +0800

    initial

---
+++ src/Dotnet.Core.WindowsServices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dotnet.Core.WindowsServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
